5f7c4adc5c72acfc5fea4eb56bf4812372c5f8e2
Javier Valles <deliverst@icloud.com>
2022-02-05 16:21:19 -0500

    fix project

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,1 +1,15 @@
-Notes.js
+import { MdOutlineDeleteForever } from "react-icons/md";
+
+const Note = () => {
+    return(
+    <div className="note">
+        <span>Hello this is  our first note</span>
+        <div className="note-footer">
+            <span>13/05/2022</span>
+            <MdOutlineDeleteForever className={'delete-icon'} size={'1.3em'} />
+        </div>
+    </div>
+    )
+}
+
+export default Note
