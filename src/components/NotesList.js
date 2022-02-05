08fda8894a9d9f45b034d619f2954fe26254b1a3
Javier Valles <deliverst@icloud.com>
2022-02-05 16:17:05 -0500

    create the basic structure of notes

---
+++ src/components/NotesList.js
@@ -0,0 +1,15 @@
+import Note from './Note'
+
+const NotesList = () => {
+    return <div className="notes-list">
+        <div>Lista</div>
+        <Note/>
+        <Note/>
+        <Note/>
+        <Note/>
+        <Note/>
+    </div>
+
+}
+
+export default NotesList
